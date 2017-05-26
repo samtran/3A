555c0e6a9a65bf0185c520b9d7b8c416e2048fe3
Samantha Tran <sammie_6495@yahoo.com>
2017-05-26 09:14:25 -0700

    Update lab3a.c

--- lab3a.c
+++ lab3a.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv){
+  //check arg
+  if(argc != 2){
+   fprintf(stderr, "invalid argument. Required file");
+    exit(1);
+  }
+  
+    
+}

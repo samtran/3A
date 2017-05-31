0fec2562a03ae31fac92fcbf10b20a8e2d82a6d4
Samantha Tran <samanthatran@Samanthas-MacBook-Pro-5.local>
2017-05-31 10:47:55 -0700

    GROUP option finished

--- lab3a.c
+++ lab3a.c
@@ -16,9 +16,9 @@ int main(int argc, char *argv[]){
   int fileFD = open(argv[1], O_RDONLY); //open file given                                                                                   
   if(fileFD == -1){
     fprintf(stderr, "Error opening file.");
-    exit(1);
+    exit(2);
   }
-  //Superblock parsing                                                                                                                               
+  //SUPERBLOCK PARSING                                                                                                                               
   //get block count                                                                                                                                  
   int block_count;
   pread(fileFD, &block_count, 4, 1024 + 4);
@@ -42,5 +42,40 @@ int main(int argc, char *argv[]){
   pread(fileFD, &first_inode, 4, 1024 + 84);
   //Write CSV for superblock                                                                                                                         
   fprintf(stdout, "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n", block_count, inodes_count, block_size, inode_size, block_per_group, inodes_per_group, first_inode);
+  
+  //GROUP PARSING
+  //calculate total number of groups by: total blocks/blocks per group
+  int tot_groups = block_count/block_per_group;
+  int i = 0;
+  for( i = 0; i <= tot_groups; i++){
+    if(tot_groups != 0 && i == tot_groups)
+      break;
+    int groupOffset = 1024 + 1024; //offset from superblock and size of superblock
+    groupOffset = groupOffset + (i * 32);//32 is the size of each group
+    //get free blocks
+    int freeblocks;
+    pread(fileFD, &freeblocks, 2, groupOffset + 12);
+    //get free inodes
+    int freeinodes;
+    pread(fileFD, &freeinodes, 2, groupOffset + 14);
+    //get block num of free block bitmap
+    int freeBlockBitmap;
+    pread(fileFD, &freeBlockBitmap, 4, groupOffset);
+    //get free inode bitmap 
+    int freeInodeBitmap;
+    pread(fileFD, &freeInodeBitmap, 4, groupOffset + 4);
+    //get first block of inodes
+    int firstInode;
+    pread(fileFD, &firstInode, 4, groupOffset + 8);
+    //Write CSV for group                                                                                                                        
+    //if its not the last group, its constant blocks_per_group from super block
+    if(i == (tot_groups -1))
+      fprintf(stdout, "GROUP,%i,%d,%d,%d,%d,%d,%d,%d\n", i, block_count, inodes_per_group, freeblocks, freeinodes, freeBlockBitmap, freeInodeBitmap, firstInode);
+    else{
+      int blocks_per_last_group = block_count% block_per_group;
+      fprintf(stdout, "GROUP,%i,%d,%d,%d,%d,%d,%d,%d\n", i, blocks_per_last_group, inodes_per_group, freeblocks, freeinodes, freeBlockBitmap, freeInodeBitmap, firstInode);
+    }
+      
+    }
   exit(0); 
 }
